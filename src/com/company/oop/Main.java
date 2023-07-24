174dc2bf23886aa9a4bfd3a52834cef7c220ca52
Vasko <vaskopoh@gmail.com>
2023-07-24 18:21:41 +0300

    first commit

---
+++ src/com/company/oop/Main.java
@@ -0,0 +1,15 @@
+package com.company.oop;
+
+public class Main {
+    public static void main(String[] args) {
+ /*       MyList<Integer> list = new MyListImpl<>(7);
+
+        System.out.println(list.capacity());
+        System.out.println(list.size());
+        list.add(1);
+        System.out.println(list.size());
+        System.out.println(list.indexOf(1));
+
+  */
+    }
+}
